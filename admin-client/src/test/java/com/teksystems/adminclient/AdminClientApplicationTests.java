236a0414464e3f5720b7dbc4242e383e2d96158b
Makhal.W <baruur.co@gmail.com>
2022-04-12 21:41:38 -0500

    Initial commit

---
+++ admin-client/src/test/java/com/teksystems/adminclient/AdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teksystems.adminclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
